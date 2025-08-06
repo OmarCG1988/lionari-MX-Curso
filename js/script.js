a54e5509a5781cb26e1e0ee16325f1bc0cbb9746
OmarCamachoGuerrero <ocamachoguer@gmail.com>
2025-08-05 21:47:14 -0600

    Clase 001 - HTML

--- js/script.js
+++ js/script.js
@@ -1,2 +1,3 @@
-console.log("Lionari script loaded");
-alert("Welcome to Lionari!");
+let minombre = "Omar Camacho";
+let miEdad = 25;
+console.log("Hola, mi nombre es " + minombre + " y tengo " + miEdad + " años.");
